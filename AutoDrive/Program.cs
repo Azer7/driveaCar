0d728057e547825db1a5da1caae97cb5b2264551
Azzer7 <azer.balayev@gmail.com>
2018-03-05 15:54:48 +0400

    first commit

---
+++ AutoDrive/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your full Name please");
+            string name= Console.ReadLine();
+            WeDrive gogogo = new WeDrive(name);
+
+            string select;
+            do
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Driver:"+" " +name);
+                Console.WriteLine("1. Drive");
+                Console.WriteLine("2. Add Fuel");
+                Console.WriteLine("3. Local Km");
+                Console.WriteLine("4. Global Km");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine(" ");
+                select = Console.ReadLine();
+                switch (Convert.ToInt32(select))
+                {
+                    case 1:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("You have choosen to Drive");
+                        Console.WriteLine("How many Km you want to Drive?");
+                        string drivekm = Console.ReadLine();
+                        int driveKm = 0;
+                        while (!int.TryParse(drivekm,out driveKm))
+                        {
+                            Console.WriteLine("Error. Integers only ");
+                            drivekm = Console.ReadLine();
+                            
+                        }
+                        gogogo.CalcKM(driveKm);
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("You have choosen to Add some Fuel");
+                        Console.WriteLine("How many Lt of Fuel you want to Add?");
+                        string addfuel = Console.ReadLine();
+                        int addFuel = 0;
+                        while (!int.TryParse(addfuel, out addFuel))
+                        {
+                            Console.WriteLine("Error. Integers only ");
+                            addfuel = Console.ReadLine();
+                        }
+                        gogogo.AddFuel(addFuel);
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("You have choosen Local Km");
+                        Console.WriteLine("Do you want to Reset Local Km? Y/N");
+                        string yesorno = Console.ReadLine();
+                        if (yesorno == "Y")
+                        {
+                            Console.WriteLine("You have reset Local Km ");
+                            gogogo.ResetLocal(yesorno);
+                        }
+                        else
+                        {
+                            gogogo.LocalKm();
+                        };
+                        break;
+                    case 4:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("You have choosen Global Km");
+                        gogogo.GlobalKm();
+                        Console.WriteLine("\n");
+                        break;
+                }
+            } while (Convert.ToInt32(select) != 0);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AutoDrive/WeDrive.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDrive
+{
+    class WeDrive
+    {
+        public string fullname;
+        public int driveKM = 0;
+        public int fuel = 140;
+        public int addfuel = 0;
+        public int spentFuel = 0;
+        public int globalKM = 0;
+        public int localKM = 0;
+        public int fuelСonsumption /*per 100km*/ = 20;
+        public string yesorno = "N";
+
+        public WeDrive(string fullname)
+        {
+            this.fullname = fullname;
+        }
+
+
+        public void CalcKM(int drivekm)
+        {
+            this.spentFuel = drivekm * fuelСonsumption / 100;
+
+            if (fuel> spentFuel)
+            {
+                this.driveKM = drivekm;
+                this.localKM += drivekm;
+                this.globalKM += drivekm;
+                this.fuel -= this.spentFuel;
+                Console.WriteLine("==============================");
+                Console.WriteLine("You Drive" + " " + driveKM + " " + "Km");
+                Console.WriteLine("You have spent" + " " + spentFuel + " " + "Lt of Fuel");
+                Console.WriteLine(fuel + " " + "Lt of Fuel left");
+                Console.WriteLine("\n");
+                Console.WriteLine("Local Km is" + " " + localKM + " " + "Km");
+                Console.WriteLine("Global Km is" + " " + globalKM + " " + "Km");
+                Console.WriteLine("==============================");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough fuel for this trip");
+                Console.WriteLine(fuel + " " + "Lt of Fuel left");
+            }
+        }
+
+        public void AddFuel(int addFuel)
+        {
+            this.addfuel = addFuel;
+            string leftspace = Convert.ToString(140 - fuel);
+            if (addFuel < 140 - fuel)
+            {
+                this.fuel += addFuel;
+                Console.WriteLine("Now you have" +" "+ fuel+" " +"Lt of Fuel");
+            }
+            else
+            {
+                Console.WriteLine("You have space only for"+" " +  leftspace + " "+"Lt of fuel");
+            }
+
+        }
+
+        public void ResetLocal(string Yesorno)
+        {
+            this.yesorno = Yesorno;
+            if (this.yesorno == "Y" )
+            {
+                localKM = 0;
+            };
+        }
+
+        public void GlobalKm()
+        {
+            Console.WriteLine("Global Km is" + " " + globalKM + " " + "Km");
+        }
+        public void LocalKm()
+        {
+            Console.WriteLine("Local Km is" + " " + localKM + " " + "Km");
+        }
+    }
+
+
+}
